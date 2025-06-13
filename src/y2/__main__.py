97de4405c468a21751555ec3a5b854d99dacdbab
Yilei Yang <hi@mangoumbrella.com>
2025-06-12 22:21:14 -0700

    Add the y2 hig command for http://mastodon.social/@daily_hig.

--- src/y2/__main__.py
+++ src/y2/__main__.py
@@ -1,12 +1,14 @@
 import cyclopts
 
 import y2
+from y2 import hig
 
 app = cyclopts.App(
     name="y2",
     help="Why have two when one will do?",
     version=y2.__version__,
 )
+app.command(hig.app)
 
 
 if __name__ == "__main__":
