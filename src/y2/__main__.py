25eca9151b8761686ec6fc4b0973e27f514316a6
Yilei Yang <hi@mangoumbrella.com>
2025-06-29 11:53:17 -0700

    Add a `y2 pv [PACKAGE]` command to print the installed PyPI package version.

--- src/y2/__main__.py
+++ src/y2/__main__.py
@@ -3,6 +3,7 @@ import cyclopts
 import y2
 from y2 import clean
 from y2 import hig
+from y2 import pv
 from y2 import xcode
 
 app = cyclopts.App(
@@ -13,6 +14,7 @@ app = cyclopts.App(
 app.command(hig.app)
 app.command(xcode.app)
 app.command(clean.clean)
+app.command(pv.pv)
 
 
 if __name__ == "__main__":
--- src/y2/_tempdirs.py
+++ src/y2/_tempdirs.py
@@ -6,7 +6,7 @@ def get_tempdir_root() -> pathlib.Path:
     return pathlib.Path(tempfile.gettempdir()) / "y2"
 
 
-def mkdtemp(prefix: str | None =None) -> pathlib.Path:
+def mkdtemp(prefix: str | None = None) -> pathlib.Path:
     root = get_tempdir_root()
     root.mkdir(exist_ok=True)
     return pathlib.Path(tempfile.mkdtemp(prefix=prefix, dir=root))
